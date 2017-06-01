fd2bd132d5f505958d6e721cc97e309dab0cf2b0
Jean Lucas <jeanleonion@gmail.com>
2017-06-01 12:09:28 -0300

    Added sw support and manifest.json

---
+++ config/manifest.js
@@ -0,0 +1,19 @@
+/*jshint node:true*/
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "live-voting-system",
+    short_name: "live-voting-system",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+    ]
+  };
+}
